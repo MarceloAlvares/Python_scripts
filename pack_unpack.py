4683d87a14afcec398d782ee07fd891c1bea8bb1
Marcelo <caccelli@yahoo.com.br>
2023-10-23 20:57:22 -0300

    Pack and unpack

--- pack_unpack.py
+++ pack_unpack.py
@@ -1,25 +1,32 @@
-tupla1 = (1,1,1)
-lista2 = [2,2,2]
+tupla1 = (1,2,3)
+lista2 = [1,2,3]
 
+# Funcao soma limitada com arametros posicionais
 def soma(x,y,z):
     return x+y+z
 
+# Funcao soma com * para packing ou unpacking
 def soma_n(*numeros):
-    soma = 0
-    for soma in numeros:
-        soma += soma
-    return soma
+    somaf = 0
+    print(type(numeros)) # mesmo passando lista ele sempre converte para tupla
+    for somaf in numeros:
+        somaf += somaf
+    return somaf
 
 # Modo normal passando os 3 parametros posicionais da funcao soma.
-print(soma(1,1,1))
+print(soma(1,2,3))
 
-# Modo Unpacking sera extraido os valores da tupla e da lista para funcao soma.
+# Modo Unpacking sera extraido os valores da tupla e da lista para  as funcoes de soma.
 print(soma(*tupla1))
-print(soma(*lista2))
+print(soma_n(*tupla1))
 
+print(soma(*lista2))
+print(soma_n(*lista2))
 
 # Modo Packing onde ele recebe os valores avulsos posicionais e carrega dentro uma tupla
-#   da funcao para executar o algoritmo de soma.
+# interna dentro funcao para executar o algoritmo de soma.
 
 print(soma_n(1,2,3))
+
+
 
